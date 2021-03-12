35bc6a89c82d614479e056f01a73e0ba09b3007d
binna <every5116@naver.com>
2021-03-13 00:15:37 +0900

    기본 세팅

---
+++ src/main/java/com/makeus/makeushackathon/src/user/UserController.java
@@ -0,0 +1,13 @@
+package com.makeus.makeushackathon.src.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.makeus.makeushackathon.config.BaseResponseStatus.*;
+
+@AllArgsConstructor
+@RestController
+@CrossOrigin
+public class UserController {
+}
