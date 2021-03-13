2ee9f22f198b052b9414d5dc94ebbf098622fbf2
nays111 <43735448+nays111@users.noreply.github.com>
2021-03-13 11:20:12 +0900

    feat : 게시물 상세 조회 API (댓글 같이 나옴)

---
+++ src/main/java/com/makeus/makeushackathon/utils/TimeDiff.java
@@ -0,0 +1,31 @@
+package com.makeus.makeushackathon.utils;
+
+
+class TIME_MAXIMUM {
+
+    public static final int SEC = 60;
+    public static final int MIN = 60;
+    public static final int HOUR = 24;
+    public static final int DAY = 30;
+    public static final int MONTH = 12;
+}
+public class TimeDiff {
+    public String timeDiff(long regTime,long curTime){
+        String createdDayBefore="";
+        long diffTime = (curTime-regTime)/1000;
+        if(diffTime < TIME_MAXIMUM.SEC) {
+            createdDayBefore = diffTime + "초전";
+        } else if ((diffTime /= TIME_MAXIMUM.SEC) < TIME_MAXIMUM.MIN) {
+            createdDayBefore = diffTime + "분전";
+        } else if ((diffTime /= TIME_MAXIMUM.MIN) < TIME_MAXIMUM.HOUR) {
+            createdDayBefore = (diffTime ) + "시간전";
+        } else if ((diffTime /= TIME_MAXIMUM.HOUR) < TIME_MAXIMUM.DAY) {
+            createdDayBefore = (diffTime ) + "일전";
+        } else if ((diffTime /= TIME_MAXIMUM.DAY) < TIME_MAXIMUM.MONTH) {
+            createdDayBefore = (diffTime ) + "달전";
+        } else {
+            createdDayBefore = (diffTime) + "년전";
+        }
+        return createdDayBefore;
+    }
+}
